b30c1cfd184c5ac75b4b1b86d5b800b6df9c975b
Nicolás Hernandez <nico220916@gmail.com>
2025-12-16 15:07:23 -0500

    Conectar frontend con backend en Render

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://help-desk-frontend-ipfk.onrender.com";
 
 const form = document.getElementById("ticketForm");
 const lista = document.getElementById("listaTickets");
